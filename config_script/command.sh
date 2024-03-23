7f001dd3e0d265dbaaabf06fc09edf84665ded76
yuanke wei <weiyuanke123@gmail.com>
2024-03-23 10:06:43 +0800

    m

--- config_script/command.sh
+++ config_script/command.sh
@@ -1,12 +1,21 @@
 echo "start configuring proxy"
-./socket_to_http/socks2http -http 8080 -socks 9002 &
-sleep 20
 rm -f ${HOME}/.ssh/known_hosts;
-HTTP_PROXY=http://127.0.0.1:8080 HTTPS_PROXY=http://127.0.0.1:8080 open -n /Applications/Utilities/Terminal.app
-IFS=$'\n'
-for svc in `networksetup -listallnetworkservices`; do
-    if [[ $svc != *"a network service is disabled"* ]];then
-        networksetup -setsocksfirewallproxy ${svc} 127.0.0.1 9002
-    fi
-done
+
+if [[ "$OSTYPE" =~ ^darwin ]];then
+  ./socket_to_http/socks2http -http 8080 -socks 9002 &
+  sleep 20
+  HTTP_PROXY=http://127.0.0.1:8080 HTTPS_PROXY=http://127.0.0.1:8080 open -n /Applications/Utilities/Terminal.app
+  IFS=$'\n'
+  for svc in `networksetup -listallnetworkservices`; do
+      if [[ $svc != *"a network service is disabled"* ]];then
+          networksetup -setsocksfirewallproxy ${svc} 127.0.0.1 9002
+      fi
+  done
+fi
+
+if [[ "$OSTYPE" =~ ^linux ]];then
+  gsettings set org.gnome.system.proxy.socks host '127.0.0.1'
+  gsettings set org.gnome.system.proxy.socks port 9002
+fi
+
 sshpass -p Admin123 ssh -o StrictHostKeyChecking=no -D 9002 root@${PubIP} iftop -t -n
