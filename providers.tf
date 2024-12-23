827d6725355c6a7307f0bb44693fadd6f1f54b45
yuanke wei <weiyuanke123@gmail.com>
2024-12-23 20:24:07 +0800

    update

--- providers.tf
+++ providers.tf
@@ -79,7 +79,7 @@ resource "alicloud_instance" "instance" {
   provisioner "remote-exec" {
     inline = [
       "sudo apt-get update",
-      "sudo apt-get install -y iftop docker.io openssl libssl-dev golang",
+      "sudo apt-get install -y iftop docker.io",
     ]
   }
   provisioner "local-exec" {
